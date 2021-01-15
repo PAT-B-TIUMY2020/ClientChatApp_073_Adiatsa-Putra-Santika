cbeb5811808201e191c2adcf26e18c97fff55d02
AdiatsaPutra <santikosetyo77@gmail.com>
2021-01-16 04:51:32 +0700

    Add project files.

---
+++ ClientChatApp_073_Adiatsa Putra Santika/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientChatApp_073_Adiatsa_Putra_Santika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InstanceContext context = new InstanceContext(new ClientCallback());
+            serviceReference1.ServiceCallbackClient server = new serviceReference1.ServiceCallbackClient(context);
+
+            Console.WriteLine("Enter Username");
+            string nama = Console.ReadLine();
+            server.gabung(nama);
+
+            Console.WriteLine("Kirim Pesan");
+            string pesan = Console.ReadLine();
+
+            while (true)
+            {
+                if (!string.IsNullOrEmpty(pesan))
+                    server.kirimPesan(pesan);
+                Console.WriteLine("Kirim Pesan");
+                pesan = Console.ReadLine();
+            }
+        }
+        public class ClientCallback : ServiceReference1.IserviceCallbackCallback
+        {
+            public void pesanKirim(string user, string pesan)
+            {
+                Console.WriteLine("{0}: {1}", user, pesan);
+            }
+        }
+    }
+}
